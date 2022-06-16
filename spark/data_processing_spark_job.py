a1fe2158bdaa7176bf8ac06b044e672c4f19f010
be_aleks <bella.aleksander@gmail.com>
2022-06-16 15:01:15 +0200

    1. Add the authors aggregate 2. Add the project presentation

--- spark/data_processing_spark_job.py
+++ spark/data_processing_spark_job.py
@@ -1,6 +1,9 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, split, to_date
+from pyspark.sql.functions import col, split, to_date, explode, collect_set, sum, avg
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DoubleType
+
+MONGODB_URI = 'mongodb://localhost:27017/'
+MONGODB_DB = 'bgd'
 
 spark = SparkSession\
     .builder\
@@ -25,20 +28,41 @@ data_schema = StructType([
     StructField("publisher", StringType(), True)
 ])
 
-raw_data = spark\
+raw_books = spark\
     .read\
-    .schema(data_schema)\
-    .csv(f'data/books.csv')
+    .csv('data/books.csv', schema=data_schema, header=True)
 
-data = raw_data\
+books = raw_books\
+    .withColumnRenamed('bookID', 'book_id')\
     .withColumn('authors', split(col('authors'), '/'))\
-    .withColumn('publication_date', to_date(col('publication_date'), 'M/d/yyyy'))
+    .withColumn('publication_date', to_date(col('publication_date'), 'M/d/yyyy'))\
+    .persist()
 
-data\
+authors = books\
+    .withColumn('author', explode('authors'))\
+    .groupby('author')\
+    .agg(
+        collect_set('book_id').alias('book_ids'),
+        collect_set('publisher').alias('publishers'),
+        avg('average_rating').alias('books_average_rating'),
+        avg('num_pages').alias('books_num_pages_avg'),
+        sum('ratings_count').alias('books_ratings_count_sum'),
+        sum('text_reviews_count').alias('books_text_reviews_count_sum'))
+
+books\
     .write\
     .format("mongo")\
     .mode("overwrite")\
-    .option("uri", "mongodb://localhost:27017/")\
-    .option("database", "local")\
+    .option("uri", MONGODB_URI)\
+    .option("database", MONGODB_DB)\
     .option("collection", "books")\
+    .save()
+
+authors \
+    .write \
+    .format("mongo") \
+    .mode("overwrite") \
+    .option("uri", MONGODB_URI) \
+    .option("database", MONGODB_DB) \
+    .option("collection", "authors") \
     .save()
