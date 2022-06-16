dafe1a5a14bc7923629d57303f7f3dc78c02454b
cezary-pokropek <cezary.pokropek@gmail.com>
2022-06-16 19:56:14 +0200

    Gat request only for books

--- routes/book.py
+++ routes/book.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 # from models.book import Book 
 from config.db import conn 
-from schemas.book import serializeDict # , serializeList
+from schemas.book import serializeDict #, serializeList
 from bson import ObjectId
 book = APIRouter() 
 
@@ -9,6 +9,6 @@ book = APIRouter()
 async def find_book_by_id(book_id):
     return serializeDict(conn.bgd.books.find_one({"_id":ObjectId(book_id)}))
 
-@book.get('/{author_id}')
-async def find_author_by_id(author_id):
-    return serializeDict(conn.bgd.authors.find_one({"_id":ObjectId(author_id)}))
+# @book.get('/{author_id}')
+# async def find_author_by_id(author_id):
+#     return serializeDict(conn.bgd.authors.find_one({"_id":ObjectId(author_id)}))
